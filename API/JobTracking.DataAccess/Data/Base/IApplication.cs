c4cc37fdec916cf3ce6fa199271fc44146a64196
Martin Belchev <MDBelchev21@codingburgas.bg>
2025-05-19 00:40:11 +0300

    Add Models

---
+++ API/JobTracking.DataAccess/Data/Base/IApplication.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracking.DataAccess.Data.Base
+{
+    public enum ApplicationStatus
+    {
+        Submitted,
+        Interview,
+        Rejected,
+        Accepted
+    }
+
+    public interface IApplication
+    {
+        int Id { get; set; }
+
+        int UserId { get; set; }
+        int JobPostingId { get; set; }
+
+        ApplicationStatus Status { get; set; }
+    }
+}
